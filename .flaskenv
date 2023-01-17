bd8ccb6e2b7a3d10a0974ccf43c4832be58fa504
JamesOmare <jamesomare922@gmail.com>
2023-01-17 03:43:44 +0300

    removed .flaskenv file

--- .flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=website
-FLASK_DEBUG=False
--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from website.config.config import Config
 app = create_app(Config)
 
 if __name__ == '__main__':
-    app.run(port = 5005)
+    app.run(port = 5000)
